ccd1de32ce82250e8c9b277f3387443bf97b400c
sbaidachni <abdullahknz@gmail.com>
2017-12-16 17:52:52 +0300

    initial

--- app.js
+++ app.js
@@ -1,12 +1,91 @@
 // app.js
 angular.module('sortApp', [])
 
+
 .controller('mainController', function($scope,$http,$timeout) {
-  
+    $scope.specialCoins = [];
+    $scope.mycoins = [
+        "ADA",
+         "XEM",
+         "Omg",
+         "Waves",
+         "TRX",
+         "BTS",
+         "bytecoin",
+         "steem",
+         "komodo",
+         "salt",
+         "doge",
+         "ark",
+         "mco",
+         "game",
+         "bnt",
+         "MTL",
+         "nav", 
+        "edg",
+         "storj",
+         "ppc", 
+        "ubq", 
+        "zen",
+         "grs",
+         "ant",
+         "adx",
+         "ftc",
+         "part",
+         "qrl",
+         "rise",
+         "fair", 
+        "via",
+         "nmc",
+         "emc",
+         "cloak", 
+        "agrs",
+         "aeon",
+         "eng",
+         "TIX", 
+        "ION",
+         "SIB",
+         "gup",
+         "ok",
+         "xel", 
+        "SHIFT", 
+        "BLK", 
+        "VRC", 
+        "TRIG", 
+        "golos",
+         "GCR", 
+        "RBY", 
+        "DTB", 
+        "VIB", 
+        "THC", 
+        "AMP",
+         "TX", 
+        "BSD",
+         "Rads", 
+        "neos",
+         "clam",
+         "myst",
+         "slr",
+         "btm",
+         "exp",
+         "swt", 
+        "xvc",
+        "NCNT"
+    ]
     $scope.reload = function () {
         $http.get("https://api.coinmarketcap.com/v1/ticker/")
         .then(function(response) {
             $scope.sushi = response.data;
+            $scope.specialCoins = [];
+            $scope.olmayanCoinler = [];
+            $scope.sushi.forEach(element => {
+                for(i=0;i<$scope.mycoins.length;i++){
+                    if($scope.mycoins[i].toUpperCase()==element.symbol.toUpperCase() || $scope.mycoins[i].toUpperCase()==element.name.toUpperCase()){
+                        $scope.specialCoins.push(element);
+                    }
+                    
+                }
+            });
         }); 
     
         $timeout(function(){
@@ -18,12 +97,6 @@ $scope.sortType     = 'name'; // set the default sort type
 $scope.sortReverse  = false;  // set the default sort order
 $scope.searchFish   = '';     // set the default search/filter term
 
-// create the list of sushi rolls 
-/* $scope.sushi = [
-{ name: 'Cali Roll', fish: 'Crab', tastiness: 2 },
-{ name: 'Philly', fish: 'Tuna', tastiness: 4 },
-{ name: 'Tiger', fish: 'Eel', tastiness: 7 },
-{ name: 'Rainbow', fish: 'Variety', tastiness: 6 }
-]; */
+
 
 });
