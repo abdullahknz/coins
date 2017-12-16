0680b06c98fe30e0d39683a3263a0d5ba0c6aa85
sbaidachni <abdullahknz@gmail.com>
2017-12-16 15:34:27 +0300

    initial

---
+++ app.js
@@ -0,0 +1,28 @@
+// app.js
+angular.module('sortApp', [])
+
+.controller('mainController', function($scope,$http,$timeout) {
+    $scope.reload = function () {
+        $http.get("https://api.coinmarketcap.com/v1/ticker/")
+        .then(function(response) {
+            $scope.sushi = response.data;
+        }); 
+    
+        $timeout(function(){
+          $scope.reload();
+        },5000)
+      };
+      $scope.reload();
+$scope.sortType     = 'name'; // set the default sort type
+$scope.sortReverse  = false;  // set the default sort order
+$scope.searchFish   = '';     // set the default search/filter term
+
+// create the list of sushi rolls 
+/* $scope.sushi = [
+{ name: 'Cali Roll', fish: 'Crab', tastiness: 2 },
+{ name: 'Philly', fish: 'Tuna', tastiness: 4 },
+{ name: 'Tiger', fish: 'Eel', tastiness: 7 },
+{ name: 'Rainbow', fish: 'Variety', tastiness: 6 }
+]; */
+
+});
